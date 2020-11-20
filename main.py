88694c0294a479492a4023133a94693b4f7ef38a
Evgenia003 <73337022+Evgenia003@users.noreply.github.com>
2020-11-20 19:11:59 +0300

    Update main.py
    в 9, 10, 11, 12, 13 строчках лишние пробелы
    в 17, 18, 19 строчках убрала табуляции
    в 19 строчке добавила скобку
    22, 24, 26 строчки - лишние пробелы
    24 строчка изменено имя
    25 поменяла знак после равно

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ while True:
           break
      else:
           userList.append(int(listElem))
-
+          
 print('\n1 - Print the first and last list item')
 print('2 - Print the sum of all items')
-     print('3 - Print amount of all list items')
-     print('\nSelect an option by typing the character: ')
-     option = int(input()
+print('3 - Print amount of all list items')
+print('\nSelect an option by typing the character: ')
+option = int(input())
 
 if option == 1:
-     module1.first_and_last_ite(userList)
+     module1.first_and_last_item(userList)
 elif option == 2:
-     module2.lis_sum(userList)
-elif option == 4:
+     module2.sum_of_all_items(userList)
+elif option == 3:
      module3.amount_of_all_items(userList)
