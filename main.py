3507dd7826d141ff2d20ea8edbbbefd78b84c34a
Alla41 <73988011+Alla41@users.noreply.github.com>
2020-11-20 15:18:11 +0300

    Update main.py
    убрала лишние пробелы

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import module1
-import module2.py
+import module2
 import module3
-
 print('List created...\n')
-
 userList = [] 
 while True:
      listElem = input('Input list element ')
@@ -11,13 +9,11 @@ while True:
           break
      else:
           userList.append(int(listElem))
-
 print('\n1 - Print the first and last list item')
 print('2 - Print the sum of all items')
-     print('3 - Print amount of all list items')
-     print('\nSelect an option by typing the character: ')
-     option = int(input()
-
+print('3 - Print amount of all list items')
+print('\nSelect an option by typing the character: ')
+option = int(input()
 if option == 1:
      module1.first_and_last_ite(userList)
 elif option == 2:
